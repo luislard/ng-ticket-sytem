846f17873b15943e85272d27ca3e0cc79976f294
Luis Rosales <luis.rosales@solutions30.com>
2017-12-05 11:45:17 +0100

    just created ticket folder and ticket-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TicketListComponent } from './tickets/ticket-list/ticket-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketListComponent
   ],
   imports: [
     BrowserModule
